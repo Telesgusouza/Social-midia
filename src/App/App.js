e41967a747d7a9f9419bd5a299c9086803f2c2b3
Gustavo Teles <gustavo.teles711@gmail.com>
2022-11-02 22:36:02 -0300

    Adicionado o header da aplicação

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 import * as Styled from "./style";
 import theme from "../Data/theme.json";
-import Header from "../components/Header";
 
 function App() {
   const [themePage, setThemePage] = useState(false);
 
-  console.log(theme.light.backgroundDown);
+  function handleInputTheme() {
+    setThemePage((p) => !p);
+  }
+
+  console.log(theme.dark.text1);
 
   return (
     <Styled.Container>
@@ -18,7 +21,31 @@ function App() {
         bg={themePage ? theme.light.backgroundDown : theme.dark.backgroundDown}
       />
 
-      <Header />
+      <Styled.ContainerContent className="center">
+        <Styled.CotainerHeader
+          theme={themePage ? theme.light.text2 : theme.dark.text2}
+        >
+          <Styled.titleHeader
+            colorSpan={themePage ? theme.light.text1 : theme.dark.text1}
+          >
+            <h1>Painel de Rede Social</h1>
+            <span>total de seguidores: 23,004</span>
+          </Styled.titleHeader>
+          <Styled.ContainerInput>
+            <div>
+              {themePage ? "Light" : "Dark"} Mode
+              <Styled.InputTheme
+                onClick={handleInputTheme}
+                color={themePage ? "white" : "black"}
+                toggle={themePage ? theme.light.toggle : theme.dark.toggle}
+                onOff={themePage ? 0 : 70}
+              >
+                <div></div>
+              </Styled.InputTheme>
+            </div>
+          </Styled.ContainerInput>
+        </Styled.CotainerHeader>
+      </Styled.ContainerContent>
     </Styled.Container>
   );
 }
--- src/App/style.js
+++ src/App/style.js
@@ -5,12 +5,20 @@ export const Container = styled.div`
   position: relative;
 `;
 
+export const ContainerContent = styled.div`
+  z-index: 20;
+  color: white;
+  position: relative;
+`;
+
 export const bgUp = styled.div`
   position: absolute;
   top: 0;
   width: 100%;
   height: 30%;
-  background-color: ${props=>props.bg};
+  background-color: ${(props) => props.bg};
+
+  transition: all 0.2s ease-in-out;
 `;
 
 export const bgDown = styled.div`
@@ -18,5 +26,86 @@ export const bgDown = styled.div`
   bottom: 0;
   width: 100%;
   height: 71%;
-  background-color: ${props=>props.bg};
+  background-color: ${(props) => props.bg};
+
+  transition: all 0.2s ease-in-out;
+`;
+
+// HEADER
+
+export const CotainerHeader = styled.header`
+  color: ${(props) => props.theme};
+
+  padding-top: 25px;
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 460px) {
+    flex-direction: column;
+  }
+`;
+
+export const titleHeader = styled.div`
+h1 {
+    font-size: 1.5em;
+  }
+
+  span {
+    font-size: 0.8em;
+    font-weight: 700;
+
+    color: ${(props) => props.colorSpan};
+  }
+
+  @media (max-width: 460px) {
+    padding: 12px 0 15px 0;
+  }
+`;
+
+export const ContainerInput = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    font-size: 0.8em;
+
+    color: hsl(230, 22%, 74%);
+
+    font-weight: 700;
+  }
+  @media (max-width: 460px) {
+    padding: 16px 0;
+    border-top: 1px solid hsl(230, 22%, 74%);
+    div {
+      justify-content: space-between;
+    }
+  }
+`;
+
+export const InputTheme = styled.div`
+  background-image: linear-gradient(to right, ${(props) => props.toggle});
+
+  margin-left: 7px;
+
+  width: 40px;
+  padding: 5px;
+  border-radius: 10px;
+
+  transition: all 0.2s ease-in-out;
+
+  position: relative;
+
+  cursor: pointer;
+
+  div {
+    background-color: ${(props) => props.color};
+
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+
+    transition: all 0.2s ease-in-out;
+
+    position: relative;
+    left: ${(props) => props.onOff}%;
+  }
 `;
